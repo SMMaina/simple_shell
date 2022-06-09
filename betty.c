34f1413b61b3f54640b12cb552099b1a9f02a43e
Cindy Muraya <cmuraya12@gmail.com>
2022-06-09 10:08:28 -0700

    task 0

---
+++ betty.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints an integer
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int n;
+
+	printf("Enter an integer: ");
+	scanf("%d", &n);
+	printf("You entered: %d", n);
+
+	return (0);
+}
