417f9e174e95dcfa28761f78fbe16020602e22d5
Cindy Muraya <cmuraya12@gmail.com>
2022-06-15 06:47:55 -0700

    Simple shell

--- builtins.c
+++ builtins.c
@@ -15,28 +15,3 @@ void exit_shell(char **args, char *line, char **env)
 	(void)env;
 	exit(0);
 }
-
-
-/**
- * env_shell - prints the current environment
- * @args: arguments
- * @line: line buffer of user input
- * @env: a NULL terminated array of strings
- */
-
-void env_shell(char **args, char *line, char **env)
-{
-	int size, i;
-
-	i = 0;
-
-	while (env[i] != NULL)
-	{
-		size = _strlen(env[i]);
-		write(1, env[i], size);
-		write(1, "\n", 1);
-		i++;
-	}
-	(void)args;
-	(void)line;
-}
